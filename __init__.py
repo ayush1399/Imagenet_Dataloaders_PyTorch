44c82a3db0b9b7ac870401dc67139f3ac06f6ba6
Ayushman Singh <git@ayushmansingh.com>
2023-11-26 06:07:05 -0500

    fix imports

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
 from .Imagenet1K import I1KTest as Imagenet1KTest
 from .Imagenet1K import I1KVal as Imagenet1KVal
 from .ImagenetA import ImagenetA
+from .ImagenetC import IC as ImagenetC
 from .ImagenetR import ImagenetR
 from .ImagenetV2 import IV2 as ImagenetV2
+
+__all__ = [
+    "Imagenet1KTest",
+    "Imagenet1KVal",
+    "ImagenetA",
+    "ImagenetC",
+    "ImagenetR",
+    "ImagenetV2",
+]
